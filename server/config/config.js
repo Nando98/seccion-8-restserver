4327bf43bb422d277ff2c37b4116049b80ce7309
Fernando <fer.munoz.alvarez@gmail.com>
2019-01-06 14:03:05 -0500

    Fix db path

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123456$F@ds161304.mlab.com:61304/cafe';
+    urlDB = 'mongodb://cafe-user:123456F@ds161304.mlab.com:61304/cafe';
 }
 
 process.env.URLDB = urlDB;
