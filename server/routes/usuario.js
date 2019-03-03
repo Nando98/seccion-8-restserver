a657272dfb080812dc274e40f2cd2249ca712725
nando98 <fer.munoz.alvarez@gmail.com>
2019-03-03 00:30:51 -0500

    fix

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -6,6 +6,14 @@ const { verificarToken, verificarRol } = require('../middleware/auth');
 
 const app = express();
 
+
+app.get('/status', (req, res) => {
+    res.json({
+        ok: true,
+        message: 'Servicios en funcionamiento'
+    })
+});
+
 app.get('/usuarios', verificarToken, (req, res) => {
 
     let desde = req.query.desde || 0;
@@ -13,14 +21,14 @@ app.get('/usuarios', verificarToken, (req, res) => {
     let hasta = req.query.hasta || 5;
     hasta = Number(hasta);
 
-    Usuario.find({estado: true}, 'nombre email role estado google img') // El segundo argumento sirve para poder colocar los atritubos que creamos convenientes
-        .skip( desde )
-        .limit( hasta )
-        .exec( (err, usuarios) => {
-            if( err ) {
-                return res.status(400).json({ok: false, err});
+    Usuario.find({ estado: true }, 'nombre email role estado google img') // El segundo argumento sirve para poder colocar los atritubos que creamos convenientes
+        .skip(desde)
+        .limit(hasta)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({ ok: false, err });
             }
-            Usuario.count({estado: true}, (err, conteo) => {
+            Usuario.count({ estado: true }, (err, conteo) => {
                 res.json({
                     ok: true,
                     usuarios,
@@ -38,16 +46,16 @@ app.post('/usuario', [verificarToken, verificarRol], (req, res) => {
     let usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: bcrypt.hashSync( body.password, 10 ),
+        password: bcrypt.hashSync(body.password, 10),
         role: body.role
     });
 
-    usuario.save( (err, usuarioDB) => {
-        if( err ) {
-            return res.status(400).json({ok: false, err});
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({ ok: false, err });
         }
 
-        res.json({ok: true, usuario: usuarioDB});
+        res.json({ ok: true, usuario: usuarioDB });
     });
 });
 
@@ -55,12 +63,12 @@ app.post('/usuario', [verificarToken, verificarRol], (req, res) => {
 app.put('/usuario/:id', verificarToken, (req, res) => {
 
     let id = req.params.id;
-    let body = _.pick(req.body, ['nombre','email','img','role','estado']);
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
-    Usuario.findByIdAndUpdate( id, body, {new: true, runValidators: true}, (err, usuarioDB) => {
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
 
-        if( err ) {
-            return res.status(400).json({ok: false, err});
+        if (err) {
+            return res.status(400).json({ ok: false, err });
         }
 
         res.json({
@@ -75,12 +83,12 @@ app.delete('/usuario/:id', verificarToken, (req, res) => {
 
     let id = req.params.id;
 
-    Usuario.findByIdAndRemove( id, (err, usuarioBorrado) => {
-        if( err ) {
-            return res.status(400).json({ok: false, err});
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({ ok: false, err });
         }
-        if( !usuarioBorrado ) {
-            return res.status(400).json({ok: false, err: {message: 'Usuario no encontrado'}});
+        if (!usuarioBorrado) {
+            return res.status(400).json({ ok: false, err: { message: 'Usuario no encontrado' } });
         }
 
         res.json({
@@ -95,10 +103,10 @@ app.delete('/usuario-flag/:id', verificarToken, (req, res) => {
     let id = req.params.id;
     let change = { estado: false };
 
-    Usuario.findByIdAndUpdate( id, change, {new: true}, (err, usuarioBorrado) => {
+    Usuario.findByIdAndUpdate(id, change, { new: true }, (err, usuarioBorrado) => {
 
-        if( err ) {
-            return res.status(400).json({ok: false, err});
+        if (err) {
+            return res.status(400).json({ ok: false, err });
         }
 
         res.json({
